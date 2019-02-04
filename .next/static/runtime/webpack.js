907568e814d99d860395ddcb47e839ad47e393b7
Mizuno Sugu <mimizunosuguru@gmail.com>
2019-02-04 10:52:47 +0900

    refactoring global css

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9d122c9230e5fb2cd25f";
+/******/ 	var hotCurrentHash = "b6b635d8d58efee440ef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Layout.js
+++ components/Layout.js
@@ -4,42 +4,7 @@ import  Footer from './Footer'
 import styled from 'styled-components';
 
 const Body = styled.div`
-  background: #fff;
-  font-family: 'Avenir', 'YuGothic';
   
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  .link:hover {
-    cursor: pointer;
-    opacity: 0.6;
-  }
-
-  p {
-    font-size: 17px;
-    line-height: 1.8;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  a:hover {
-    opacity: 0.6;
-    cursor: pointer;
-  }
-
-  a:visited {
-    color: #000;
-    text-decoration: none;
-  }
-
-  li {
-    list-style-type: none;
-  }
 `
 
 const Layout = (props) => (
@@ -47,6 +12,47 @@ const Layout = (props) => (
     <Header/>
     {props.children}
     <Footer/>
+    <style>{`
+    body {
+      margin: 0;
+      background: #fff;
+      font-family: 'Avenir', 'YuGothic';
+    }
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+      .link:hover {
+      cursor: pointer;
+      opacity: 0.6;
+    }
+
+      p {
+      font-size: 17px;
+      line-height: 1.8;
+    }
+
+      a {
+      text-decoration: none;
+    }
+
+      a:hover {
+      opacity: 0.6;
+      cursor: pointer;
+    }
+
+      a:visited {
+      color: #000;
+      text-decoration: none;
+    }
+
+      li {
+      list-style-type: none;
+    }
+      `}</style>
   </Body>
 )
 
